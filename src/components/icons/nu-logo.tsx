1c4587c1260c62e94c7ecda332df01cc24bf74a2
João Victor <joaovictorvilasboasdesouza1@gmail.com>
2025-06-26 04:51:19 +0000

    Ainda não esta igual

--- src/components/icons/nu-logo.tsx
+++ src/components/icons/nu-logo.tsx
@@ -3,12 +3,16 @@ export function NuLogo() {
     <svg
       aria-hidden="true"
       height="32"
-      viewBox="0 0 110 95"
+      viewBox="0 0 111 96"
       xmlns="http://www.w3.org/2000/svg"
       className="text-primary"
       fill="currentColor"
     >
-      <path d="M 5,95 V25 C 5,5 65,5 65,25 V70 C 65,95 105,95 105,70 V20 H85 V70 C 85,85 55,85 55,70 V25 C 55,15 25,15 25,25 V95 Z" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5 95.0001L5 25.0001C5 5.00006 65 5.00006 65 25.0001L65 70.0001C65 95.0001 105 95.0001 105 70.0001V20.0001H85V70.0001C85 85.0001 55 85.0001 55 70.0001V25.0001C55 15.0001 25 15.0001 25 25.0001V95.0001H5Z"
+      />
     </svg>
   );
 }
