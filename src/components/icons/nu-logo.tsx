0d8c03d910549508b2af547c1820c63961018e11
João Victor <joaovictorvilasboasdesouza1@gmail.com>
2025-06-26 04:34:27 +0000

    Quero que use essa imagem como logo tipo do aplicativo

--- src/components/icons/nu-logo.tsx
+++ src/components/icons/nu-logo.tsx
@@ -1,7 +1,15 @@
 export function NuLogo() {
   return (
-    <span className="text-4xl font-bold tracking-tighter text-primary">
-      nu
-    </span>
+    <svg
+      aria-hidden="true"
+      height="32"
+      viewBox="0 0 205 120"
+      xmlns="http://www.w3.org/2000/svg"
+      className="text-primary"
+      fill="currentColor"
+    >
+      <path d="M 10 100 C 10 0, 110 0, 110 100 L 90 100 C 90 20, 30 20, 30 100 Z" />
+      <path d="M 95 20 C 95 120, 195 120, 195 20 L 175 20 C 175 100, 115 100, 115 20 Z" />
+    </svg>
   );
 }
